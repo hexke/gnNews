77270bafde60977a5aa362cfcca4c5f789faf33c
Krzysztof_Karowiec <krzysztof.karowiec@gmail.com>
2023-03-24 09:18:02 +0100

    updated README, added articles count store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,4 @@
+import countSlice from "./countSlice";
 import { displaySlice } from "./displaySlice";
 
 import { configureStore } from '@reduxjs/toolkit';
@@ -5,10 +6,12 @@ import { configureStore } from '@reduxjs/toolkit';
 const store = configureStore({
     reducer: {
         display: displaySlice.reducer,
+        count: countSlice.reducer,
     }
 });
 
 export const displayActions = displaySlice.actions;
+export const countActions = countSlice.actions;
 
 export type TRootState = ReturnType<typeof store.getState>;
 
